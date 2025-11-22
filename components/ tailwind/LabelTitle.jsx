218eb948a9d702768b8e2999dfb14f25e82e6da6
rebecavasco-prog <rebeca.vasco@estudante.ifb.edu.br>
2025-11-22 18:30:40 -0300

    Create  LabelTitle.jsx

---
+++ components/ tailwind/LabelTitle.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LabelTitle({ texto }) {
+  return (
+    <label className="block text-2xl font-bold text-blue-800 mb-4 text-center">
+      {texto}
+    </label>
+  );
+}
